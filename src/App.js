1bbd13ed62d9c256e21863865ccdc951fed7aea5
= <=>
2024-06-29 12:33:34 +0400

    how to make react function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
+import meet from './meet.svg'
 import './App.css';
+
+function Header(props) {
+  return (
+    <header className="App-header">
+      <img src={props.logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {props.linkLabel}
+      </a>
+    </header>
+  )
+}
+
+function Jinu() {
+  return(
+    <Header linkLabel="My name is Jchat" logo={logo}></Header>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header linkLabel="Nice to meet you" logo={meet}></Header>
+      <Jinu></Jinu>
     </div>
   );
 }
